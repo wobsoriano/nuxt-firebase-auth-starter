2b42a921cc47a8ec0eea4329ced48a63fbf7c9cd
Robert Soriano <sorianorobertc@gmail.com>
2022-05-02 16:23:48 -0700

    fix cookie access in server

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,10 @@
-import type { IncomingMessage, ServerResponse } from 'http'
-import { useCookie } from 'h3'
 import { getApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
-export default  defineEventHandler(async(event) => {
+export default defineEventHandler(async(event) => {
   const { req, res } = event
   if (req.url.includes('/api/')) {
-    const token = useCookie(req, 'token')
+    const {token} = useCookies(event)
     const app = getApp()
     const auth = getAuth(app)
     try {
